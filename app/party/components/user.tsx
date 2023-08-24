fce69b403a5a1d04d41053b7d5370429fc04dc0a
ComboRush <comborush.init@proton.me>
2023-08-24 14:41:09 +0300

    user componenet for the party page
    the title says it all i guess

---
+++ app/party/components/user.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default () => {
+    return(
+        <main className="relative m-2 border-4 w-[140px] h-[180px] rounded-lg text-white overflow-hidden">
+            <Image className="flex ml-auto mr-auto mt-2 border-0 rounded-full" width={128} height={128} src="/fire.jpg" alt="Avatar Logo"></Image>
+            <h1 className="flex w-fit h-fit ml-auto mr-auto font-bold text-[23px]">Fire</h1>
+        </main>
+    )
+}
